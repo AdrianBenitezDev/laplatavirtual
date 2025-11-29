021c4375f6d1127b2432941f44ed9508d6e74a9e
Adrian Dev <artbenitezDev@gmail.com>
2025-11-29 07:37:33 -0300

    ip

--- public/regitrar.js
+++ public/regitrar.js
@@ -1,25 +1,30 @@
 const host = location.hostname.replace("www.", "");
 
-
 if (host === "laplatavirtual.com.ar") {
 
-  let userID = localStorage.getItem("lpvID");
+  fetch("https://api.ipify.org?format=json")
+    .then(r => r.json())
+    .then(dataip => {
 
-  if (!userID) {
-    userID = Math.floor(10000000 + Math.random() * 90000000) + "_lpv";
-    localStorage.setItem("lpvID", userID);
-  }
+      let userID = localStorage.getItem("lpvID");
 
-  const url =
-    "https://script.google.com/macros/s/AKfycbxSXhwvoTeYPd2Ju74Fibufr6_lrtfxzpYqFw12QVBuigKwjJJ2vjmgGdjScCmVaU2MWQ/exec" +
-    "?id=" + encodeURIComponent(userID);
+      if (!userID) {
+        userID = Math.floor(10000000 + Math.random() * 90000000) + "_lpv";
+        localStorage.setItem("lpvID", userID);
+      }
 
-  fetch(url) // GET por defecto
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+      const url =
+        "https://script.google.com/macros/s/AKfycbxSXhwvoTeYPd2Ju74Fibufr6_lrtfxzpYqFw12QVBuigKwjJJ2vjmgGdjScCmVaU2MWQ/exec" +
+        "?id=" + encodeURIComponent(userID) +
+        "&ip=" + encodeURIComponent(dataip.ip);
 
+      fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
 
-}else{
-  console.log("se encuentra en version desarrollador")
+    });
+
+} else {
+  console.log("se encuentra en version desarrollador");
 }
